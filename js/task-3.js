4abbb80fdb9fba96f6a8e5c28e961f894f51d13e
Roman Soltan <romabandit88@gmail.com>
2024-09-20 23:24:52 +0300

    hw-js-1

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  const boxSizing =
+    Number.parseFloat(content) +
+    2 * (Number.parseFloat(padding) + Number.parseFloat(border));
+  return Number.parseFloat(boxSizing);
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
